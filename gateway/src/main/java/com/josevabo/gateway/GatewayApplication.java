cd8c44f94096a55d1c80672cad6a769d6849a132
josevabo <43576654+josevabo@users.noreply.github.com>
2022-12-22 17:37:50 -0300

    api gateway: working for clientes/, pacientes/, medicos
    - eg.: localhost:8080/pacientes

--- gateway/src/main/java/com/josevabo/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/josevabo/gateway/GatewayApplication.java
@@ -2,12 +2,48 @@ package com.josevabo.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class GatewayApplication {
 
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/medicos/**")
+						.filters(f ->f.circuitBreaker(config -> config
+								.setName("mycmd")
+								.setFallbackUri("forward:/fallback"))
+						)
+						.uri("http://localhost:8082")
+				)
+				.route(p -> p
+						.path("/consultas/**")
+						.filters(f ->f.circuitBreaker(config -> config
+								.setName("mycmd")
+								.setFallbackUri("forward:/fallback"))
+						)
+						.uri("http://localhost:8081")
+				)
+				.route(p -> p
+								.path("/pacientes/**")
+								.uri("http://localhost:8083")
+				)
+				.build();
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@RequestMapping("/fallback")
+	public Mono<String> fallback() {
+		return Mono.just("Aplicação fora do ar");
 	}
 
 }
