38f11b0e4daafb8d33833e614a5610bd08c330fe
josevabo <43576654+josevabo@users.noreply.github.com>
2022-12-22 17:40:53 -0300

    testconfig project: created

---
+++ testconfig/src/main/java/com/example/testconfig/TestconfigApplication.java
@@ -0,0 +1,13 @@
+package com.example.testconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestconfigApplication.class, args);
+	}
+
+}
---
+++ testconfig/src/test/java/com/example/testconfig/TestconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
